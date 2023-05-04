0938d1019d4877e82ea6c31d6d5531d4b2885e7e
Gxorge <github@ghott.me>
2023-05-04 20:01:23 +0100

    Readability changes

--- src/main/java/me/sekalol15/SilkSpawnersOG/SilkSpawnersOG.java
+++ src/main/java/me/sekalol15/SilkSpawnersOG/SilkSpawnersOG.java
@@ -1,16 +1,18 @@
 package me.sekalol15.SilkSpawnersOG;
-import me.sekalol15.SilkSpawnersOG.out.production.SilkSpawnersOG.me.sekalol15.SilkSpawnersOG.rightClickListener;
+
 import org.bukkit.plugin.java.JavaPlugin;
+
 //todo list
 //add option to change spawner type with head
 //add option to change drop chance
 //possibly add a option to make different spawner quality levels
 public class SilkSpawnersOG extends JavaPlugin {
+
     @Override
     public void onEnable() {
-        this.getServer().getPluginManager().registerEvents(new breakBlockListener(), this);
-        this.getServer().getPluginManager().registerEvents(new rightClickListener(), this);
-        getServer().getConsoleSender().sendMessage("functioning");
+        this.getServer().getPluginManager().registerEvents(new BlockEventListener(), this);
+        System.out.println("Ready!");
     }
+
 }
 
