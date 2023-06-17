61009c9b7aa46a60916bbcecae77e5e80f2ac5b3
Sekalol15 <92968224+Sekalol15@users.noreply.github.com>
2023-06-17 09:54:15 +0200

    Update SilkSpawnersOG.java
    change todo list

--- src/main/java/me/sekalol15/SilkSpawnersOG/SilkSpawnersOG.java
+++ src/main/java/me/sekalol15/SilkSpawnersOG/SilkSpawnersOG.java
@@ -6,6 +6,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 //add option to change spawner type with head
 //add option to change drop chance
 //possibly add a option to make different spawner quality levels
+//possibly add messages when a spawner doesn‘t drop and vice versa
 public class SilkSpawnersOG extends JavaPlugin {
 
     @Override
